61ef8ad1a1af4c8c9c7195f4ce6706da7edaa418
Ta1Vos <302196386@student.rocmondriaan.nl>
2022-11-21 10:29:22 +0100

    JS Part of hover (BUGS)
    Warning: Contains bugs.
    
    Try to fix the textcontent reader in the click function

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ let playField = [false, false, false, false, false, false, false, false, false];
 let playerTurn = 1;
 let fieldOccupation = 0;
 let winValue = false;
+let blockClicked = false;
 
 const winCondition = [
     //Horizontal
@@ -23,6 +24,12 @@ const winCondition = [
 for (i = 1; i < 10; i++) {
     let currentNumber = i;
     document.querySelector(`.block${currentNumber}`).addEventListener(`click`, placeFigure);
+    document.querySelector(`.block${currentNumber}`).addEventListener(`mouseenter`, function() {
+        blockEnter(currentNumber);
+    });
+    document.querySelector(`.block${currentNumber}`).addEventListener(`mouseleave`, function() {
+        blockLeave(currentNumber);
+    });
 }
 
 function checkWin(symbol) {
@@ -54,20 +61,22 @@ function checkWin(symbol) {
 
 function placeFigure(clickEvent) {
     fieldOccupation++;
-    blockClick = clickEvent.target.textContent;
-    currentNumber = blockClick - 1;
-    console.log(blockClick);
-    const gridItemLocation = document.querySelector(`.block${blockClick}`);
+    blockNumber = clickEvent.target.textContent;
+    currentNumber = blockNumber - 1;
+    console.log(blockNumber);
+    // const gridItemLocation = document.querySelector(`.block${blockNumber}`);
     if (playerTurn == 1) {
+        blockClicked = true;
         playerTurn = 2;
-        document.querySelector(`.block${blockClick}`).removeEventListener(`click`, placeFigure);
-        document.querySelector(`.block${blockClick}`).innerHTML = `<img src="img/X.png" alt="X" height="175px" width="175px">`;
+        document.querySelector(`.block${blockNumber}`).removeEventListener(`click`, placeFigure);
+        document.querySelector(`.block${blockNumber}`).innerHTML = `<img src="img/X.png" alt="X" height="175px" width="175px">`;
         playField[currentNumber] = `X`;
         checkWin(`X`);
     } else if (playerTurn == 2) {
+        blockClicked = true;
         playerTurn = 1;
-        document.querySelector(`.block${blockClick}`).removeEventListener(`click`, placeFigure);
-        document.querySelector(`.block${blockClick}`).innerHTML = `<img src="img/O.png" alt="O" height="175px" width="175px">`;
+        document.querySelector(`.block${blockNumber}`).removeEventListener(`click`, placeFigure);
+        document.querySelector(`.block${blockNumber}`).innerHTML = `<img src="img/O.png" alt="O" height="175px" width="175px">`;
         playField[currentNumber] = `O`;
         checkWin(`O`);
     }
@@ -86,3 +95,27 @@ function placeFigure(clickEvent) {
 // }
 
 // switchHover();
+
+function removeHoverListeners(blockNumber) {
+    document.querySelector(`.block${blockNumber}`).removeEventListener(`mouseenter`, function() {
+        blockEnter(currentNumber);
+    });
+    document.querySelector(`.block${blockNumber}`).removeEventListener(`mouseleave`, function() {
+        blockLeave(currentNumber);
+    });
+}
+
+function blockEnter(blockNumber) {
+    if (playerTurn == 1) {
+        document.querySelector(`.block${blockNumber}`).innerHTML += `<img src="img/X.png" alt="X" height="175px" width="175px">`;
+    } else if (playerTurn == 2) {
+        document.querySelector(`.block${blockNumber}`).innerHTML += `<img src="img/O.png" alt="O" height="175px" width="175px">`;
+    }
+}
+
+function blockLeave(blockNumber) {
+    if (blockClicked == false) {
+        document.querySelector(`.block${blockNumber}`).innerHTML = blockNumber;
+    }
+    blockClicked = false;
+}
