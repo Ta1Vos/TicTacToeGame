2e620a0d061cf8cd7425bb5bfe1cafe0405b3c15
Ta1Vos <302196386@student.rocmondriaan.nl>
2023-06-06 15:52:29 +0200

    renamed index to home.html

--- js/sharedFunctions.js
+++ js/sharedFunctions.js
@@ -66,7 +66,7 @@ if (twoPlayers == true) {
 
 //Sends the player back to the index page
 function backToHome() {
-    window.location = `index.html`;
+    window.location = `/home.html`;
 }
 
 //Opens the reset popup.
